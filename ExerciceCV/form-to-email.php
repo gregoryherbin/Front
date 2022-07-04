f36c0d4d95c70a2166614f44c9c58b863304fd38
GreG <greghdev@outlook.fr>
2022-07-04 16:43:12 +0200

    04/07/22 exo CV v5

---
+++ ExerciceCV/form-to-email.php
@@ -0,0 +1,20 @@
+<!-- <?php  -->
+// if(isset($_POST['submit'])){
+    // $to = "greghdev@outlook.fr"; // this is your Email address
+    // $email = $_POST['email']; // this is the sender's Email address
+    // $prenom = $_POST['prénom'];
+    // $nom = $_POST['nom'];
+    // $objet = "Form submission";
+    // $subject2 = "Copy of your form submission";
+    // $message = $prenom . " " . $nom . " wrote the following:" . "\n\n" . $_POST['message'];
+    // $message2 = "Here is a copy of your message " . $prenom . "\n\n" . $_POST['message'];
+
+    // $headers = "From:" . $email;
+    // $headers2 = "From:" . $to;
+    // mail($to,$objet,$message,$headers);
+    // mail($email,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    // echo "Mail envoyé, merci " . $prenom . ", je vous contacte rapidement";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
+    // }
+// ?>
