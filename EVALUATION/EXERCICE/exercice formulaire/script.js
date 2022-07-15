6985b423dc7e471da4c5b51c0638fb4a99156cab
GreG <greghdev@outlook.fr>
2022-07-15 12:01:14 +0200

    formulaire 15/07/22

---
+++ EVALUATION/EXERCICE/exercice formulaire/script.js
@@ -0,0 +1,5 @@
+function concat() {
+var nom = document.getElementById("in").value;
+var prenom = document.getElementById("in2").value;
+document.getElementById("out").value = nom + prenom;
+}
--- Javascript/exojava/exercice formulaire/formulaire.js
+++ Javascript/exojava/exercice formulaire/formulaire.js
@@ -1,4 +1,10 @@
 function checkForm(f) {
-    alert("Vous vous appelez: " + f.elements["chp"].value + " " + f.elements["chp2"].value + " et vous avez"+ " " + f.elements["chp3"].value + " ans");
-    return false;
+    chp = document.form1[0].value;
+    chp2 = document.form1[1].value;
+    result = form1[0].value + " " + form1[1].value;
 }
+document.write(result)
+
+
+// alert("Vous vous appelez: " + f.elements["chp"].value + " " + f.elements["chp2"].value + " et vous avez"+ " " + f.elements["chp3"].value + " ans");
+    // return false;
